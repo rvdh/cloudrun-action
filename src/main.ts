0ea8fb141c0e188128d718c5737560cab99f3ed0
Rick van den Hof <r.vandenhof@ultimaker.com>
2020-08-24 15:17:49 +0200

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,66 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const image: string = core.getInput('image')
+    const name: string = core.getInput('name')
+    const serviceAccountName: string = core.getInput('serviceAccountName')
+    const vpcConnectorName: string = core.getInput('vpcConnectorName')
+    core.debug(`Deploying docker image ${image}...`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const {google} = require('googleapis')
+    const run = google.run('v1')
 
-    core.setOutput('time', new Date().toTimeString())
+    // Obtain user credentials to use for the request
+    const auth = new google.auth.GoogleAuth({
+      scopes: [
+        'https://www.googleapis.com/auth/cloud-platform',
+      ],
+    })
+
+    const authClient = await auth.getClient()
+    google.options({auth: authClient})
+
+    const projectId = await google.auth.getProjectId()
+
+    const res = await run.namespaces.services.create({
+      requestBody: {
+        metadata: {
+          name: name,
+        },
+        spec: {
+          template: {
+            spec: {
+              serviceAccountName: serviceAccountName,
+              annotations: {
+                "run.googleapis.com/vpc-access-connector": vpcConnectorName
+              },
+
+              containers: [{
+                image: image,
+                env: [{
+                  name: 'ENV_NAME',
+                  value: 'cloudrun',
+                }]
+              ]},
+            },
+          },
+        },
+      }
+    })
+
+    console.log(res.data)
+
+    // add github comment
+    // update comment (checking for image)
+    // wait for image
+    // timed out
+    // update comment
+    // gcloud run deploy
+    // update comment
+
+
+    //core.setOutput('url', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
