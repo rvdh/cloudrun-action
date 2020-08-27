c4bb77d6f2c46085c7ac8c57573d3370d45d7119
Rick van den Hof <r.vandenhof@ultimaker.com>
2020-08-27 09:18:14 +0200

    fixup! Initial version

--- src/gcloud.ts
+++ src/gcloud.ts
@@ -94,7 +94,7 @@ export async function waitForDockerImage(
   google.options({auth: authClient})
   const project = await auth.getProjectId()
 
-  const token = await auth.getToken()
+  const token = await authClient.getToken()
   //const axios = require('axios').default
 
   const imageUrl = new URL(`https://${image}`)
