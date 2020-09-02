0e45770a5f954374b85c3e19a057c83e4eb6a6f7
Rick van den Hof <r.vandenhof@ultimaker.com>
2020-09-02 15:00:28 +0200

    fixup! Initial version

--- src/docker.ts
+++ src/docker.ts
@@ -5,11 +5,24 @@ export async function getEnvVarsFromImage(
 ): Promise<string[] | undefined> {
   const Docker = require('dockerode')
   const docker = new Docker({socketPath: '/var/run/docker.sock'})
+  const serviceAccountKey: string = core.getInput('service_account_key', {
+    required: true
+  })
+
+  const imageUrl = new URL(`https://${name}`)
+
+  const auth = {
+    username: '_json_key',
+    password: serviceAccountKey,
+    auth: '',
+    email: '',
+    serveraddress: `https://${imageUrl.host}/v2`
+  }
+
   try {
-    const image = await docker.getImage(name)
+    const image = await docker.pull(name, {authconfig: auth})
     const imageInspect = image.inspect()
     core.info(JSON.stringify(imageInspect, null, 4))
-
     core.info(JSON.stringify(image, null, 4))
     return (await image).Config?.Env
   } catch (error) {
